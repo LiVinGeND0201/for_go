66a8504dae79880a1d64492b5a4d56fa7a6183b0
nbm <nbm@163.com>
2023-03-19 19:18:12 +0800

    first commit

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+import "fmt"
+ 
+func main() {
+	var cmd string
+	for {
+		fmt.Print("Please input a command: ")
+		fmt.Scanln(&cmd)
+		switch cmd {
+		case "help":
+			fmt.Println("This is help command.")
+		case "quit":
+			fmt.Println("Bye.")
+			return
+		default:
+			fmt.Println("Wrong command!1111")
+		}
+	}
+}
